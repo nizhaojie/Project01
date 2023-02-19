b4f524910915827e8ad895ae95685b47f549c8e4
nzj <2472154484@qq.com>
2023-02-19 14:44:23 +0800

    2022/02/19

--- project/src/server/db/index.js
+++ project/src/server/db/index.js
@@ -5,8 +5,8 @@ let db = mysql.createPool({
   host: 'localhost', //数据库IP地址
   port: '3306',
   user: 'root', //数据库登录账号
-  password: 'n20010907', //数据库登录密码
-  database: 'project' //要操作的数据库
+  password: '', //数据库登录密码
+  database: '' //要操作的数据库
 })
 
 module.exports = db
